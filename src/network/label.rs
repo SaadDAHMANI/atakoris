96b755fc19e9ad2669fd4ad831bc420f0e01b8b6
SaadDAHMANI <tahatatakor7@gmail.com>
2025-11-28 20:38:36 +0100

    add Label to  Network

--- src/network/label.rs
+++ src/network/label.rs
@@ -7,32 +7,41 @@ use crate::Position;
 #[derive(Debug, Clone, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub struct Label {
     text: [char; 25],
-    pos: Position,
+    pub pos: Position,
 }
 
 impl Label {
-    pub fn from_str(s: &str) -> Option<Self> {
+    pub fn from_str(s: &str) -> Self {
         let ln = s.len();
-        if ln == 0 {
-            return None;
-        } else if ln > 25 {
-            let mut buf = ['\0'; 25];
+        let mut buf = ['\0'; 25];
+        if ln > 25 {
             for (i, c) in s.chars().take(25).enumerate() {
                 buf[i] = c;
             }
-            return Some(Label {
+            return Label {
                 text: buf,
                 pos: Position { x: 0.0, y: 0.0 },
-            });
+            };
         } else {
-            let mut buf = ['\0'; 25];
             for (i, c) in s.chars().enumerate() {
                 buf[i] = c;
             }
-            return Some(Label {
+            return Label {
                 text: buf,
                 pos: Position { x: 0.0, y: 0.0 },
-            });
+            };
+        }
+    }
+
+    pub fn update_by(&mut self, s: &str) {
+        for (i, c) in s.chars().enumerate() {
+            if i < 25 {
+                self.text[i] = c;
+            }
+        }
+
+        for i in s.len()..25 {
+            self.text[i] = '\0';
         }
     }
 
@@ -60,8 +69,7 @@ mod tests {
     fn label_more_25_chars() {
         let s = "Tank 1000m3 in local area at elevation 50m";
         let labl = Label::from_str(s);
-        let lstr = labl.unwrap_or_default().as_string();
-
+        let lstr = labl.as_string();
         let strng = format!("Tank 1000m3 in local area");
         assert_eq!(strng, lstr);
     }
@@ -70,9 +78,31 @@ mod tests {
     fn label_less_25_chars() {
         let s = "Tank 1000m3";
         let labl = Label::from_str(s);
-        let lstr = labl.unwrap_or_default().as_string();
+        let lstr = labl.as_string();
 
         let strng = format!("Tank 1000m3");
         assert_eq!(strng, lstr);
+    }
+
+    #[test]
+    fn label_update_chars() {
+        let s = "Tank 1000m3";
+        let mut labl = Label::from_str(s);
+
+        labl.update_by("Roua-Lil");
+
+        let strng = format!("Roua-Lil");
+        assert_eq!(strng, labl.as_string());
+    }
+
+    #[test]
+    fn label_update_by_more_25_chars() {
+        let s = "12345678901234567890123456789";
+        let mut labl = Label::from_str(s);
+
+        labl.update_by("159");
+
+        let strng = format!("159");
+        assert_eq!(strng, labl.as_string());
     }
 }
